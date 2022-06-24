730915de6b1aa5d406fa8a1125bb8e65456741c7
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2022-06-25 01:50:47 +0800

    Edited design but needs more polishing

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+// page interaction
+
 // footer date
 document.getElementById('date').innerHTML = new Date().getFullYear();
+
+
+// // test
+// var image = new Image();
+// image.src = 'images/scissors.png';
+// document.getElementById('user-chosen-hand').appendChild(image);
