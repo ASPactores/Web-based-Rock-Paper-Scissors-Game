b994b619ce45757a87548d0c25d8d424c475b2fa
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2022-06-25 13:37:07 +0800

    Added feature to display chosen hand

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-// page interaction
+const userHandDisplayDestination = "user-chosen-hand";
+const computerHandDisplayDestination = "computer-chosen-hand";
+var chosenHand = {};
+
+document.querySelectorAll('#hand-option').forEach(hand => 
+    hand.addEventListener('click', () => {
+        chosenHand.user = hand.value;
+        deployHand(chosenHand.user, userHandDisplayDestination);
+        computerChosenHand();
+    })
+);
+
+function computerChosenHand() {
+    let options = ['rock', 'paper', 'scissors'];
+    let hand = Math.floor((Math.random() * options.length));
+    chosenHand.computer = options[hand];
+    deployHand(chosenHand.computer, computerHandDisplayDestination);
+}
+
+function deployHand(hand, destination) {
+    let image, element;
+
+    image = new Image();
+    image.src = "images/" + hand + ".png";
+
+    element = document.getElementById(destination);
+
+    if (element.firstChild) {
+        element.removeChild(element.firstChild);
+        element.appendChild(image);
+    }
+
+    element.appendChild(image);
+}
+ 
+
+
+
+
+
 
 // footer date
 document.getElementById('date').innerHTML = new Date().getFullYear();
