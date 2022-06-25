345c3facc9f24bdec68dd4fa60b684a198bcba43
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2022-06-25 14:28:38 +0800

    Added scoreboard but need fixing
    Scoreboard display "NaN"

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const userHandDisplayDestination = "user-chosen-hand";
 const computerHandDisplayDestination = "computer-chosen-hand";
 var chosenHand = {};
+var score = {
+    user: 0,
+    computer: 0,
+    adjustScore: (comparisonResult) => {
+        switch(comparisonResult) {
+            case 'win':
+                this.user++;
+                break;
+            case 'lost':
+                this.computer++;
+                break;
+            default:
+                this.user++;
+                this.computer++;
+        }
+    },
+    displayScore: () => {
+        document.getElementById('score').innerHTML = this.user + " : " + this.computer;
+    }
+};
 
 document.querySelectorAll('#hand-option').forEach(hand => 
     hand.addEventListener('click', () => {
         chosenHand.user = hand.value;
         deployHand(chosenHand.user, userHandDisplayDestination);
         computerChosenHand();
+        score.adjustScore(compareHand(chosenHand.user, chosenHand.computer));
+        score.displayScore();
     })
 );
 
@@ -15,6 +37,22 @@ function computerChosenHand() {
     let hand = Math.floor((Math.random() * options.length));
     chosenHand.computer = options[hand];
     deployHand(chosenHand.computer, computerHandDisplayDestination);
+}
+
+function compareHand(userHand, computerHand) {
+    if (userHand == 'rock') {
+        if (computerHand == 'rock') return 'tie';
+        else if (computerHand == 'paper') return 'lost';
+        else return 'win';
+    } else if (userHand == 'paper') {
+        if (computerHand == 'rock') return 'win';
+        else if (computerHand == 'scissors') return 'lost';
+        else return 'tie';
+    } else { // userHand is scissors
+        if (computerHand == 'rock') return 'lost';
+        else if (computerHand == 'paper') return 'win';
+        else return 'tie';
+    }
 }
 
 function deployHand(hand, destination) {
@@ -32,7 +70,7 @@ function deployHand(hand, destination) {
 
     element.appendChild(image);
 }
- 
+
 
 
 
